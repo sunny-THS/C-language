d7ee370a9904bc76c8839a2fdff56a1802c8afc2
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-02 22:06:26 +0700

    Create bai2.cpp

---
+++ chuong3/bai2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+ int a, b;
+ printf("Nhap 2 so: ");
+ scanf("%d%d", &a, &b);
+ if(a!=b){
+  if(a>b){
+    prinf("%d > %d", a, b);
+  }else{
+    printf("%d < %d", a, b);
+  }
+ }else{
+  printf("%d = %d", a, b);
+ }
+}
