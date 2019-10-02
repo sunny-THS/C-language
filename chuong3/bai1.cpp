6c80ea76b4a0b3ca37dadbf6325b618caab6e244
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-02 21:59:01 +0700

    Update bai1.cpp

--- chuong3/bai1.cpp
+++ chuong3/bai1.cpp
@@ -2,5 +2,14 @@
 #include<conio.h>
 
 void main() {
+  int a;
+  printf("Nhap so nguyen: ");
+  scanf("%d", &a);
+  if(a>0){
+    printf("Do la so nguyen duong");
+  }else{
+    printf("Do la so nguyen am");
+  }
   
+  getch();
 }
