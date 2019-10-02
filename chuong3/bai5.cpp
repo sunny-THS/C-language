2b14da262681cc44611d0ca8cfee9c26058584ff
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-03 05:46:18 +0700

    Update bai5.cpp

--- chuong3/bai5.cpp
+++ chuong3/bai5.cpp
@@ -2,6 +2,13 @@
 #include<conio.h>
 
 void main(){
+ int a, b;
+ printf("Nhap a, b: ");
+ scanf("%d%d", a, b);
+ if(a%b==0)
+  printf("a la boi cua b");
+ else
+  printf("a khong phai la boi cua b");
  
  getch();
 }
