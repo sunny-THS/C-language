5ed89e45424c6eb852b463133667395d7683f7d6
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-03 15:09:13 +0700

    Update bai18.cpp

--- chuong3/bai18.cpp
+++ chuong3/bai18.cpp
@@ -5,7 +5,7 @@ void main(){
   int n,sum=0;
   printf("Nhap n: ");
   scanf("%d", &n);
-  for(int i=1;i>=n;i++){
+  for(int i=1;i<=n;i++){
     sum+=(float)1/(n*(n+1)*(n+2));
   }
   printf("Tong: %d", sum);
