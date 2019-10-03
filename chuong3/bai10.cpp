d7515d516ab5f11d91259662bce256d6caf8c8bd
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-03 13:34:26 +0700

    Create bai10.cpp

---
+++ chuong3/bai10.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  int nam
+  printf("Nhap 1 nam: ");
+  scanf("%d", &nam);
+  if(nam%400==0 || nam%4==0 && nam%100!=0){
+    printf("Do la nam nhuan");
+  }else{
+    printf("Do khong phai la nam nhuan");
+  }
+  getch();
+}
