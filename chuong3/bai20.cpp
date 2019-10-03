2ef64a4ff7a638e0f5ae337f9c3ec40a548cccc5
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-03 14:44:32 +0700

    Create bai20.cpp

---
+++ chuong3/bai20.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  int n,sum=0,a=0;
+  printf("Nhap n: ");
+  scanf("%d", &n);
+  for(int i=1;i>=n;i++){
+    for(int j=1;j>=i,j++){
+      a+=j;     
+    }
+    sum+=a;
+  }
+  printf("Tong: %d", sum);
+  getch();
+}
