6e2bca33197e5b6d4a712f94ec1bd1adfc1efe69
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-03 16:03:13 +0700

    Update bai20.cpp
    S

--- chuong3/bai20.cpp
+++ chuong3/bai20.cpp
@@ -2,14 +2,13 @@
 #include<conio.h>
 
 void main(){
-  int n,sum=0,a=0;
+  int n,sum=0;
   printf("Nhap n: ");
   scanf("%d", &n);
   for(int i=1;i<=n;i++){
     for(int j=1;j<=i,j++){
-      a+=j;     
+      sum+=j;     
     }
-    sum+=a;
   }
   printf("Tong: %d", sum);
   getch();
