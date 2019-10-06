ac7853cd48f58ecc9d1578de7dbd654db4136f12
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-06 09:56:47 +0700

    Create BTVN2.cpp

---
+++ btTH/BTVN2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  char tenSanPham[25];
+  int soLuong;
+  long donGia, tien;
+  float thueGTGT;
+  printf("Nhap ten san pham: ");
+  scanf("%s", tenSanPham);
+  printf("So luong: ");
+  scanf("%d", &soLuong);
+  printf("Don gia: ");
+  scanf("%ld", &donGia);
+  tien= donGia*soLuong;
+  thueGTGT= tien*0.1;
+  printf("\nTien: %ld\n", tien);
+  printf("Thue gia tri gia tang: %.2f", thueGTGT);
+  
+  getch();
+}
