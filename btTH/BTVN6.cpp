ab118eee0f87f899d251bc471e8659834b707d0c
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-06 10:43:06 +0700

    Create BTVN6.cpp

---
+++ btTH/BTVN6.cpp
@@ -0,0 +1,18 @@
+  
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  int a, b;
+  float x;
+  printf("Nhap a, b: ");
+  scanf("%d%d", &a, &b);
+  if(a!=0){
+    x= -(float)b/a;
+    printf("Nghiem cua pt bac nhat la: %.2f", x);
+  }else{
+    printf("Phuong trinh vo nghiem");
+  }
+  
+  getch();
+}
