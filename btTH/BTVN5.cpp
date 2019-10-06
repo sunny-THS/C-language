38ccf844096208fb2580f336a73c2aab0c95ec5d
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-06 10:34:38 +0700

    Create BTVN5.cpp

---
+++ btTH/BTVN5.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  char asT;
+  int asN;
+  printf("Nhap 1 ki tu: ");
+  scanf("%c", &asT);
+  asN = asT;
+  if((asN>=65&&asN<=90)||(asN>=97&&asN<=122)){
+    if(asN>=65&&asN<=90){
+      printf("Ki tu in hoa");
+    }else{
+      printf("Ki tu thuong");
+    }
+  }else{
+      printf("Ki tu khac");
+  }
+  getch();
+}
