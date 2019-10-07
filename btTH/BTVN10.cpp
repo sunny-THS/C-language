522cbfaf27bba70a6952ce95500165b0521ed0d4
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-07 15:19:08 +0700

    Update BTVN10.cpp

--- btTH/BTVN10.cpp
+++ btTH/BTVN10.cpp
@@ -3,10 +3,10 @@
 
 void main() {
 	int i, bthuc;
-  printf("Gia tri cua i la ");
-  scanf("%d", &i);
-  bthuc = 20 + ++i + i++;
-  printf("Bieu thuc 20 + ++i + i++ co ket qua la: %d; i = %d", bthuc, i);
+	printf("Gia tri cua i la ");
+	scanf("%d", &i);
+	bthuc = 20 + ++i + i++;
+	printf("Bieu thuc 20 + ++i + i++ co ket qua la: %d; i = %d", bthuc, i);
 
 	getch();
 } 
