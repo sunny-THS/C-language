ea85901bec267aafb658ec20bcb1bf4bc4bc05c0
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-10 14:16:25 +0700

    sd

--- chuong3/btvn.cpp
+++ chuong3/btvn.cpp
@@ -323,10 +323,10 @@ void main() {
         printf("In bang cuu chuong\n\n");
         for(int j=1;j<=10;j++){
           for(int i=1;i<=9;i++){
-            if(i==1){
-              printf("Bang cuu chuong %d\n", i);
-            }
             printf("%d*%d=%d\t\t", i, j, i*j);
+            if (i==6) {
+              printf("\n");
+            }
           }
           printf("\n");
         }
