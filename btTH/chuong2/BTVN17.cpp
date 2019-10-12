fe541482ff5ddd25d269aa175e14c9867036ac4e
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-12 15:18:21 +0700

    r

---
+++ btTH/chuong2/BTVN17.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+  char xhC;
+  int tong=0, soCotDiem = 4;
+  float diem[4], diemTb;
+
+  printf("Nhap diem mon Thuc hanh NMLT\n");
+  printf("Nhap cot diem 1: ");
+  scanf("%f", &diem[0]);
+  printf("Nhap cot diem 2: ");
+  scanf("%f", &diem[1]);
+  printf("Nhap cot diem 3: ");
+  scanf("%f", &diem[2]);
+  printf("Nhap cot diem 4: ");
+  scanf("%f", &diem[3]);
+  for (int i = 0; i < soCotDiem; i++) {
+      tong+=diem[i];
+  }
+  diemTb = (float)tong/soCotDiem;
+  if (diemTb>=8.5) {
+    xhC = 'A';
+  }else if (diemTb>=7) {
+    xhC = 'B';
+  }
+  else if (diemTb>=5.5) {
+    xhC = 'C';
+  }
+  else if (diemTb>=4) {
+    xhC = 'D';
+  }
+  else {
+    xhC = 'F';
+  }
+  printf("\nDiem trung binh: %.2f\t<=>\t%c", diemTb, xhC);
+
+	getch();
+}
