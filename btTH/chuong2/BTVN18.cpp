3435ab6bcaebe6e58fd14b5b08d6afeb75279d4d
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-10-13 14:14:18 +0700

    as

--- btTH/chuong2/BTVN18.cpp
+++ btTH/chuong2/BTVN18.cpp
@@ -3,7 +3,41 @@
 #include<math.h>
 
 void main(){
+  char diemC;
+  int soMon = 0;
+  int diemHeB;
+  float a,b,c,d,e,f,p,o,i,u,y,t,tong,tb;
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &a, &p);
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &b, &o);
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &c, &i);
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &d, &u);
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &e, &y);
+  printf("Nhap diem trung binh va tin chi cua mon hoc thu %d: ", ++soMon);
+  scanf("%f", &f, &t);
+  tong = a+b+c+d+e+f;
+  tb = (float)tong/soMon;
   
-
+  if (tb>=8.5) {
+    diemHeB = 4;
+    diemC = 'A';
+  }else if (tb>=7) {
+    diemHeB = 3;
+    diemC = 'B';
+  }else if (tb>=5.5) {
+    diemHeB = 2;
+    diemC = 'C';
+  }else if (tb>=4) {
+    diemHeB = 1;
+    diemC = 'D';
+  }else {
+    diemHeB = 0;
+    diemC = 'F';
+  }
+  printf("\nDiem trung binh chung(he 10): %.2f\nDiem trung binh chung(he 4): %d\nDiem chu: %c", tb, diemHeB, diemC);
 	getch();
 }
--- btTH/chuong2/BTVN19.cpp
+++ btTH/chuong2/BTVN19.cpp
@@ -3,7 +3,30 @@
 #include<math.h>
 
 void main(){
-  
+  char b[10];
+  int a;
 
-	getch();
+  printf("Nhap 1 ki so thap luc nhi phan: ");
+  scanf("%s", &b);
+
+  if ((*b>=48 && *b<=57) || (*b>=65 && *b<=70)) {
+    if (*b>=48 && *b<=57) {
+      printf("Gia tri thap phan tuong ung: %s\n", b);
+    }else{
+      switch (*b) {
+        case 65: a=10;break;
+        case 66: a=11;break;
+        case 67: a=12;break;
+        case 68: a=13;break;
+        case 69: a=14;break;
+        case 70: a=15;break;
+      }
+      printf("Gia tri thap phan tuong ung: %d\n", a);
+
+    }
+  }else{
+	   printf("He thap luc phan khong dung ki tu nay");
+  }
+
+  getch();
 }
