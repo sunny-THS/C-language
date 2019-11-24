82868e354c82766a418380bd25ac2777d354644f
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-11-24 13:53:48 +0700

    Update func.cpp

--- chuong5/ChuoiKyTu/func.cpp
+++ chuong5/ChuoiKyTu/func.cpp
@@ -1,3 +1,19 @@
 #include<stdio.h>
-#include<conio.h>
+#include<string.h>
 
+int kiemTraChuoiDoi(char *str){
+   int len = strlen(str);
+   for(int i=0; i<len/2; i++)
+      if(str[i]!=str[len-i-1])
+         return 0;
+   return 1;
+}
+
+void xoaPt(char *str){
+
+}
+void xoaKhoangChanThua(char *str){
+   int len = strlen(str);
+   if(str[0]==' ')
+      
+}
