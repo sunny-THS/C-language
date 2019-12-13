f5b935821ecd7324af912da56e703bd168371fb7
Sunny <42699455+sunny-THS@users.noreply.github.com>
2019-12-13 21:52:15 +0700

    Update func.cpp

--- btTH/chuong10/bai3/func.cpp
+++ btTH/chuong10/bai3/func.cpp
@@ -12,6 +12,8 @@ typedef struct SinhVien{
 
 void nhapTT_SV(SV &a);
 void xuatTT_SV(SV a);
+float tinhDTB(SV a);
+void xepLoai(SV a);
 
 void xuatTT_SV(SV a){
   printf("%s\t%s\t%.2f\t%.2f\t%.2f\n", a.mssv, a.tenSv, a.diemT, a.diemL, a.diemH);
@@ -25,4 +27,19 @@ void nhapTT_SV(SV &a){
   fscanf(f, "%f", &a.diemL);
   fscanf(f, "%f", &a.diemH);
   fclose(f);
+}
+float tinhDTB(SV a){
+  return (float)(a.diemT+a.diemH+a.diemL)/3;
+}
+void xepLoai(SV a){
+  float dtb = tinhDTB(a);
+  if(dtb>=8){
+    orintf("Gioi");
+  }else if(dtb>=6.5){
+    printf("Kha");
+  }else if(dtb>=5){
+    printf("Trung Binh");
+  }else{
+    printf("Yeu");
+  }
 }
