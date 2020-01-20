b684b28b4471b5dc2ec0f945c1ca5b54f8bd8df2
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-01-20 08:58:43 +0700

    sd

--- C_language/baiTapBenNgoai/bai1.cpp
+++ C_language/baiTapBenNgoai/bai1.cpp
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include <windows.h>
 
+enum KHOANGCACH {
+  LENGHT_TOP = 10,
+  LENGHT_LEFT = 2
+};
+
 void gotoxy(int x, int y)
 {
     static HANDLE h = NULL;
@@ -12,30 +17,37 @@ void gotoxy(int x, int y)
 void rectangle_full(int, int);
 void rectangle(int, int);
 void tamGiac1(int);
+void tamGiac1_1(int);
 void tamGiac2(int);
+void tamGiac2_2(int);
 void tamGiac3(int);
+void tamGiac3_3(int);
 void tamGiac4(int);
+void tamGiac4_4(int);
+void tamGiacCan(int);
 
 int main() {
   int a= 5;
   int b= 8;
   rectangle_full(a, b);
-  printf("\n");
   rectangle(a, b);
-  printf("\n");
   tamGiac1(a);
-  printf("\n");
+  tamGiac1_1(a);
   tamGiac2(a);
-  printf("\n");
+  tamGiac2_2(a);
   tamGiac3(a);
-  printf("\n");
+  //tamGiac3_3(a);
   tamGiac4(a);
+  //tamGiac4_(a);
+  tamGiacCan(a);
+
 
   return 0;
 }
 
 void rectangle_full(int a, int b) {
   for (int i = 0; i < a; i++) {
+    gotoxy(0+LENGHT_LEFT, i+LENGHT_TOP);
     for (int j = 0; j < b; j++) {
       printf("* ");
     }
@@ -44,6 +56,7 @@ void rectangle_full(int a, int b) {
 }
 void rectangle(int a, int b) {
     for (int i = 0; i < a; i++) {
+      gotoxy(LENGHT_LEFT,i+LENGHT_TOP-8);
       for (int j = 0; j < b; j++) {
         if (i==0||i==a-1) {
           printf("* ");
@@ -59,6 +72,7 @@ void rectangle(int a, int b) {
 
 void tamGiac1(int a) {
   for (int i = 1; i <= a; i++) {
+    gotoxy(20+LENGHT_LEFT,i-1+LENGHT_TOP);
     for (int j = 0; j < i; j++) {
       printf("* ");
     }
@@ -68,6 +82,7 @@ void tamGiac1(int a) {
 
 void tamGiac2(int a) {
   for (int i = a; i > 0; i--) {
+    gotoxy(35+LENGHT_LEFT,a-i+LENGHT_TOP);
     for (int j = 0; j < i; j++) {
       printf("* ");
     }
@@ -77,6 +92,7 @@ void tamGiac2(int a) {
 
 void tamGiac3(int a) {
   for (int i = 1; i <= a; i++) {
+    gotoxy(45+LENGHT_LEFT,i-1+LENGHT_TOP);
     for (int j = 0; j < a; j++) {
       if (i+j>a-1) {
         printf("* ");
@@ -90,8 +106,65 @@ void tamGiac3(int a) {
 
 void tamGiac4(int a) {
   for (int i = 0; i < a; i++) {
+    gotoxy(60+LENGHT_LEFT,i+LENGHT_TOP);
     for (int j = 1; j <= a; j++) {
       if (j>i) {
+        printf("* ");
+      }else {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+}
+
+void tamGiacCan(int a){
+  for (int i = 1; i <= a; i++) {
+    gotoxy(75+LENGHT_LEFT, i-1+LENGHT_TOP);
+    for (int j = 1; j <= 2*a-1; j++) {
+      if (j<=a+(i-1) && j>=a-(i-1)) {
+        printf("* ");
+      }else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+}
+
+void tamGiac1_1(int a) {
+  for (int i = 1; i <= a; i++) {
+    gotoxy(20+LENGHT_LEFT,i-1+LENGHT_TOP-8);
+    for (int j = 0; j < i; j++) {
+      if (i>2 && i<a && j!=0 && j!=i-1) {
+        printf("  ");
+      }else {
+        printf("* ");
+      }
+    }
+    printf("\n");
+  }
+}
+
+void tamGiac2_2(int a) {
+  for (int i = a; i > 0; i--) {
+    gotoxy(35+LENGHT_LEFT,a-i+LENGHT_TOP-8);
+    for (int j = 0; j < i; j++) {
+      if (i>2 && i<a && j!=0 && j!=i-1) {
+        printf("  ");
+      }else {
+        printf("* ");
+      }
+    }
+    printf("\n");
+  }
+}
+
+void tamGiac3_3(int a) {
+  for (int i = 1; i <= a; i++) {
+    gotoxy(45+LENGHT_LEFT,i-1+LENGHT_TOP-8);
+    for (int j = 0; j < a; j++) {
+      if (i+j>a-1) {
         printf("* ");
       }else {
         printf("  ");
