d8ad4794163ce47e0b84d9dc69350b7a9125a886
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-01-29 13:51:17 +0700

    sd

--- C_language/baiTapBenNgoai/snakeGame.cpp
+++ C_language/baiTapBenNgoai/snakeGame.cpp
@@ -1,12 +1,18 @@
 #include<stdio.h>
-#include<windows.h>
-#include<string.h>
 #include<conio.h>
-#include<time.h>
+#include<string.h>
+#include<windows.h>
+#include<conio.h>
 
 #define consoleHeight 30
 #define consoleWidth 120
 
+enum KHUNGGAME {
+  xy = 2,
+  xx = consoleWidth/2,
+  yx = 1,
+  yy = consoleHeight-consoleHeight/5
+};
 enum TrangThai {UP, DOWN, LEFT, RIGHT};
 
 typedef struct animation{
@@ -15,35 +21,35 @@ typedef struct animation{
   TrangThai tt;
 }A;
 
+void draw(int, int, int, int);
 void gotoxy(int, int);
 void cls();
 void textColor(int);
-int random(int, int);
 
 int main(){
-  srand(time(NULL)); //phat sinh mau ngau nhien
-
   A e;
-  e.text[0] = 169;
+  e.text[0] = 220;
   e.text[1] = '\0';
   int slen = strlen(e.text);
-  e.x=(consoleWidth-slen)/2;
-  e.y=0;
+  e.x=(xx-slen)/2;
+  e.y=yx;
   e.speed = 100;
-  while(1){
+
+  while (1) {
     cls();
+    draw(xx, xy, yx, yy);
     gotoxy(e.x, e.y);
-    textColor(random(0,15));
+    textColor(14);
     printf("%s", e.text);
 
     //xu ly cham bien
-    if (e.y <= 0) {
+    if (e.y < yx) {
       e.tt = DOWN;
-    }else if (e.y >= consoleHeight-1) {
+    }else if (e.y > yy) {
       e.tt = UP;
-    }else if (e.x <=0) {
+    }else if (e.x < xy) {
       e.tt = RIGHT;
-    }else if (e.x >= consoleWidth-slen) {
+    }else if (e.x > xx) {
       e.tt = LEFT;
     }
 
@@ -67,7 +73,7 @@ int main(){
       e.y--;
     }else if (e.tt == RIGHT) {
       e.x++;
-    }else{
+    }else if (e.tt == LEFT) {
       e.x--;
     }
 
@@ -77,20 +83,33 @@ int main(){
 }
 
 void gotoxy(int x, int y){
-    static HANDLE h = NULL;
-    if(!h)
-      h = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD c = { x, y };
-    SetConsoleCursorPosition(h,c);
+  static HANDLE h = NULL;
+  if(!h)
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD c = { x, y };
+  SetConsoleCursorPosition(h, c);
 }
 void cls(){
   system("cls");
 }
 void textColor(int x){
-	HANDLE color;
-	color = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(color,x);
+  HANDLE color;
+  color = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(color, x);
 }
-int random(int min, int max){
-  return min + rand()%(max+1-min);
+void draw(int xx, int xy, int yx, int yy){
+  for (int i = xy; i <= xx; i++) {
+    for (int j = yx; j <= yy; j++) {
+      if (i==xx || i==xy) {
+        gotoxy(i, j);
+        printf("%c", 179);
+      }else if (j==yy) {
+        gotoxy(i, j);
+        printf("%c", 95);
+      }else if (j==yx) {
+        gotoxy(i, j);
+        printf("%c", 196);
+      }
+    }
+  }
 }
