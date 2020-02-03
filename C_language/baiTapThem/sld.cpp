488375eaa6a4b33a3b4f2f520fefbcb184885dd4
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-02-03 15:31:08 +0700

    sd

---
+++ C_language/baiTapThem/sld.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <SDL.h>
+
+
+int main(int argc, char *argv[]) {
+  // retutns zero on success else non-zero
+   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+       printf("error initializing SDL: %s\n", SDL_GetError());
+   }
+   SDL_Window* win = SDL_CreateWindow("GAME",
+                                      SDL_WINDOWPOS_CENTERED,
+                                      SDL_WINDOWPOS_CENTERED,
+                                      1000, 1000, 0);
+   while (1)
+  return 0;
+}
