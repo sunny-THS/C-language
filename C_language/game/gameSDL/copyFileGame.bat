75d1919100361621bd73f2d707469598074e73c1
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-02-12 17:14:14 +0700

    sd

--- C_language/game/gameSDL/copyFileGame.bat
+++ C_language/game/gameSDL/copyFileGame.bat
@@ -7,10 +7,9 @@ pause > nul
 
 echo.
 echo file cpp
-copy D:\github\Programming_language\C_language\game\gameSDL\GAME.cpp D:\visual\SDLGame\SDLGame /y
-copy D:\github\Programming_language\C_language\game\gameSDL\CommonFunction.cpp D:\visual\SDLGame\SDLGame /y
+copy D:\github\Programming_language\C_language\game\gameSDL\*.cpp D:\visual\SDLGame\SDLGame /y
 echo file header
-copy D:\github\Programming_language\C_language\game\gameSDL\CommonFunction.h D:\visual\SDLGame\SDLGame /y
+copy D:\github\Programming_language\C_language\game\gameSDL\*.h D:\visual\SDLGame\SDLGame /y
 echo ===================================================
 
 goto copyFile
