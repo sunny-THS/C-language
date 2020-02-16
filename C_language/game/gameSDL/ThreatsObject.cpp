a51b9f5ea619da7d426996a634a1178d257fb53f
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-02-16 20:20:41 +0700

    sd

--- C_language/game/gameSDL/ThreatsObject.cpp
+++ C_language/game/gameSDL/ThreatsObject.cpp
@@ -1,5 +1,6 @@
 #include "ThreatsObject.h"
 
+// contructor
 ThreatsObject::ThreatsObject() {
   rect_.x = SCREEN_WIDTH;
   rect_.y = SCREEN_HEIGHT*0.5;
@@ -8,10 +9,10 @@ ThreatsObject::ThreatsObject() {
   x_val_ = 0;
   y_val_ = 0;
 }
+//destructor
 ThreatsObject::~ThreatsObject() {}
 
-void ThreatsObject::InitBullet() {
-  BulletObject* p_bullet = new BulletObject();
+void ThreatsObject::InitBullet(BulletObject* p_bullet) {
   if (p_bullet != NULL) {
     bool ret = p_bullet->LoadIMG("sphere.png");
     if (ret) {
@@ -30,7 +31,7 @@ void ThreatsObject::MakeBullet(SDL_Surface* des, const int& x_limit, const int& 
       if (p_bullet->get_is_move()) {
         p_bullet->Show(des);
         p_bullet->HandleMoveRightToLeft();
-      }else {
+      }else {// duoc lap lai khi cham bien trai
         p_bullet->set_is_move(true);
         p_bullet->SetRect(rect_.x, rect_.y + rect_.h*0.5);
       }
@@ -42,7 +43,7 @@ void ThreatsObject::HandleMove(const int& border_x, const int& border_y) {
   rect_.x -= x_val_;
   if (rect_.x < 0) {
     rect_.x = SCREEN_WIDTH;
-    rect_.y = SDL_CommonFunction::Random(0, SCREEN_HEIGHT*0.75);
+    rect_.y = SDL_CommonFunction::Random(0, SCREEN_HEIGHT*0.75); 
   }
 }
 
