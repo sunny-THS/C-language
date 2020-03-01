0628100f0e28b05baf8bc5b025c3c426149d46e9
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-03-01 14:19:24 +0700

    sd

--- C_language/baiTapThem/Chuong1/bai1.cpp
+++ C_language/baiTapThem/Chuong1/bai1.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
+int Sum(int x, int y) {
+  return x + y;
+}
+int Sub(int x, int y) {
+  return x - y;
+}
+int Mul(int x, int y) {
+  return x * y;
+}
+float Div(int x, int y) {
+  float a = x;
+  return a / y;
+}
 int main() {
-   int x, y;
-   return 0;
+  int x, y;
+  cout << "Nhap 2 so x, y: ";
+  cin >> x >> y;
+  cout << "Ket qua la: " <<endl;
+  cout << "x + y= " << Sum(x, y) << endl;
+  cout << "x - y= " << Sub(x, y) << endl;
+  cout << "x * y= " << Mul(x, y) << endl;
+  cout << "x / y= " << floorf(Div(x, y)) << endl;
+  return 0;
 }
