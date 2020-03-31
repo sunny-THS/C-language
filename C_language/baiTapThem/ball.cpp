43e038dad6a7bc1490fba99a9a0e5c57f629d765
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-03-31 23:09:11 +0700

    sd

--- C_language/baiTapThem/ball.cpp
+++ C_language/baiTapThem/ball.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 #include <Windows.h>
+
+void textColor(int x) {
+  HANDLE color;
+  color = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(color, x);
+}
+void gotoxy(int x, int y) {
+  static HANDLE h = NULL;
+  if (!h) {
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+  }
+  COORD c = { x, y };
+  SetConsoleCursorPosition(h, c);
+}
