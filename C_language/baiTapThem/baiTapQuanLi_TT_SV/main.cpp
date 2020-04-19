6c5fa503b04613c7bbc284937017c44f11a205e7
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-04-19 22:11:28 +0700

    sd

--- C_language/baiTapThem/baiTapQuanLi_TT_SV/main.cpp
+++ C_language/baiTapThem/baiTapQuanLi_TT_SV/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -8,10 +9,6 @@ struct MonHoc {
   float diem;
 };
 typedef struct MonHoc MONHOC;
-struct TenSV {
-  string ho, tenDem, ten;
-};
-typedef struct TenSV TENSV;
 struct SinhVien {
   string hoTen, maSo, ngaySinh;
   vector<MONHOC> dsMonHoc;
@@ -67,21 +64,82 @@ void Xuat_TT_SV(vector<SINHVIEN> ds_sv) {
   }
 }
 void Xuat_TT_SV_Theo_Ten(vector<SINHVIEN> ds_SV, string ten_SV) {
-  for (size_t i = 0; i < ds_SV.size(); i++) {
-    TENSV tenSV;
-    vector<TenSV> ten_1_SV;
-    char *token = strtok(ten_1_SV[i].hoTen, " ");
-    while (token != NULL) {
-      
+  short int len = ds_SV.size();
+  for (size_t i = 0; i < len; i++) {
+    if (stricmp(ten_SV.c_str(), ds_SV[i].hoTen.c_str())==0) {
+      cout << "\n\t\tThong tin sinh vien" << endl;
+      Xuat_TT_1_SV(ds_SV[i]);
+      cout << "\n\t\t- DANH SACH MON HOC -" << endl;
+      for (size_t j = 0; j < ds_SV[i].dsMonHoc.size(); j++) {
+        Xuat_MonHoc_1_SV(ds_SV[i].dsMonHoc[j]);
+      }
+      cout << "\n===================================================\n";
     }
   }
 }
+string Tach_NamSinh(string str) {
+  string tmp;
+  short int index = str.length()-1;
+  while (str[index]>='0'&&str[index]<='9') {
+    tmp.insert(tmp.begin() + 0, str[index]);
+    index--;
+  }
+  return tmp;
+}
+void Ghi_TT_MonHoc(fstream &fileout, MONHOC monHoc) {
+  fileout << monHoc.ten << '-' << monHoc.diem;
+}
+void Ghi_TT_SV(fstream &fileout, SINHVIEN sv) {
+  short int len = sv.dsMonHoc.size();
+  fileout << sv.hoTen << ',' << sv.maSo << ',' << sv.ngaySinh << endl;
+  fileout << sv.dsMonHoc.size() << endl;
+  for (size_t i = 0; i < len; i++) {
+    Ghi_TT_MonHoc(fileout, sv.dsMonHoc[i]);
+    if (i<len-1) {
+      fileout << endl;
+    }
+  }
+}
+void Luu_File_NamSinh(fstream &fileout, vector<SINHVIEN> ds, string namSinh) {
+  short int d = 0;
+  for (size_t i = 0; i < ds.size(); i++) {
+    string tmp = Tach_NamSinh(ds[i].ngaySinh);
+    if (stricmp(tmp.c_str(), namSinh.c_str())==0) {
+      d++;
+    }
+  }
+  for (size_t i = 0; i < ds.size(); i++) {
+    string tmp = Tach_NamSinh(ds[i].ngaySinh);
+    if (stricmp(tmp.c_str(), namSinh.c_str())==0) {
+      Ghi_TT_SV(fileout, ds[i]);
+      d--;
+      if (d!=0) {
+        fileout << endl;
+      }
+    }
+  }
+}
+
 int main() {
   vector<SINHVIEN> ds_SV;
+  // Cau 1
   fstream filein;
   filein.open("SINHVIEN.txt", ios::in);
   Doc_File(filein, ds_SV);
+  // Xuat_TT_SV(ds_SV);
+  // Cau 2
+  string name;
+  cout << "\nNhap ten sinh vien can tim: ";
+  getline(cin, name);
+  Xuat_TT_SV_Theo_Ten(ds_SV, name);
+  // Cau 3
+  fstream fileout;
+  fileout.open("NAMSINH.txt", ios::out);
+  string namSinh;
+  cout << "Nhap nam sinh: ";
+  getline(cin, namSinh);
+  Luu_File_NamSinh(fileout, ds_SV, namSinh);
+  fileout.close();
   filein.close();
-  Xuat_TT_SV(ds_SV);
   return 0;
 }
