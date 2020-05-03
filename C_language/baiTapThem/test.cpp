d8d83ced56f551aced0ddad5d06382193560af27
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-05-03 15:41:44 +0700

    sd

--- C_language/baiTapThem/test.cpp
+++ C_language/baiTapThem/test.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <Windows.h>
 using namespace std;
+void GotoXY(short int x, short int y) {
+  HANDLE hConsoleOutput;
+  COORD Cursor_an_Pos = {x, y};
+  hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
 int main() {
-  fstream filein;
-  string a;
-  filein.open("log.txt", ios::in);
-
-  if (!filein.eof()) {
-    getline(filein, a);
+  int n = 3;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      GotoXY(j, i);
+      if (i==j || i+j==n-1) {
+        putchar(42);
+      }
+    }
   }
-  cout << a;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 5; j < n+5; j++) {
+      GotoXY(j, i);
+      if ((i==0|| i==n-1)||(!(i==0|| i==n-1)&&(j==5||j==n+5-1))) {
+        putchar(42);
+      }
+    }
+  }
+  system("pause>nul");
   return 0;
 }
