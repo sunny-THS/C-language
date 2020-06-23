799ab5e5ed22abacb21b6dd03842bd4271bb3854
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-06-23 23:32:37 +0700

    sd

---
+++ C_Sharp/BTVN/btDaiKa_Vinh/TuHueSon_2001190791/Bai11/2001190791_LopHoc.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuHueSon_2001190791
+{
+  public class LopHoc
+  {
+    List<HocSinh> List_hs_;
+    
+  }
+}
