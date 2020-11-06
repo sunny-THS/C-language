f2d399c5a5322b078ea93dbe757c3bf063d4b5f9
sunny-THS <tuhueson@gmail.com>
2020-11-06 17:14:31 +0700

    sd

--- C_language/NhapMon_C/baiTapThem/test.cpp
+++ C_language/NhapMon_C/baiTapThem/test.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include <Windows.h>
-using namespace std;
-int main() {
-  int xPos, yPos;
-  int lParam;
-  while (WM_MOUSEMOVE) {
-    xPos = GET_X_LPARAM(lParam);
-    yPos = GET_Y_LPARAM(lParam);
-  }
+#include <stdio.h>
+
+int main()
+{
+  char s[100];
+  char sa[100];
+  // scanf("%[0-9]", s);
+  printf("%s\t", s);
   return 0;
 }
