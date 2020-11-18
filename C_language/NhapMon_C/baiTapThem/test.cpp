312a10ff10efeea0aa24c03ce08372e81da4236a
sunny-THS <tuhueson@gmail.com>
2020-11-18 21:05:59 +0700

    sd

--- C_language/NhapMon_C/baiTapThem/test.cpp
+++ C_language/NhapMon_C/baiTapThem/test.cpp
@@ -2,9 +2,23 @@
 
 int main()
 {
-  char s[100];
-  char sa[100];
-  scanf("%[^\n]%*c", s);
-  printf("%s\t", s);
+  int a, b, c, d, e, f;
+  int sum;
+  // tinh tong 6 so nguyen nhap tu ban phim
+  //nhap gia tri
+  printf("a: ");
+  scanf("%d", &a);
+  printf("b: ");
+  scanf("%d", &b);
+  printf("c: ");
+  scanf("%d", &c);
+  printf("d: ");
+  scanf("%d", &d);
+  printf("e: ");
+  scanf("%d", &e);
+  printf("f: ");
+  scanf("%d", &f);
+  sum = a+b+c+d+e+f;
+  printf("Tong: %d\n", sum);
   return 0;
 }
