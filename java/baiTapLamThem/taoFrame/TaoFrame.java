42f487f598cfe2ca6dee8e4b044c57bc679de9d0
sunny-THS <tuhueson@gmail.com>
2021-04-01 10:34:09 +0700

    sd

---
+++ java/baiTapLamThem/taoFrame/TaoFrame.java
@@ -0,0 +1,18 @@
+package taoframe;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class TaoFrame extends JFrame {
+  public TaoFrame(String title) {
+    this.setTitle(title);
+    this.setSize(300, 200);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+    JButton btnThoat = new JButton("Thoát");
+    getContentPane().add(btnThoat);
+  }
+  public static void main(String[] args) {
+      new TaoFrame("vd tạo cửa sổ 1").setVisible(true);
+  }
+}
